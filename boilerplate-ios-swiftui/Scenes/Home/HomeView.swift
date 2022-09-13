c58687c792c27d8d8dadf18597e7ff5d72a72b52
Selim Gungorer <selim.gungorer@adesso.com.tr>
2022-09-14 00:41:33 +0300

    [BSU-0006] Localization files implemented

--- boilerplate-ios-swiftui/Scenes/Home/HomeView.swift
+++ boilerplate-ios-swiftui/Scenes/Home/HomeView.swift
@@ -12,7 +12,7 @@ struct HomeView: View {
     var body: some View {
         ZStack {
             Color.red
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/.localized)
         }
         .ignoresSafeArea()
     }
