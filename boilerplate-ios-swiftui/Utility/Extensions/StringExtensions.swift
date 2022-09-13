4b00576b3f066d0d8f113bda582df04728522d08
Selim Gungorer <selim.gungorer@adesso.com.tr>
2022-09-14 00:41:33 +0300

    [BSU-0006] Localization files implemented

--- boilerplate-ios-swiftui/Utility/Extensions/StringExtensions.swift
+++ boilerplate-ios-swiftui/Utility/Extensions/StringExtensions.swift
@@ -12,6 +12,14 @@ extension String {
     var localized: String {
         NSLocalizedString(self, comment: "") //swiftlint:disable:this nslocalizedstring_key
     }
+    
+    func localized(withComment comment:String, tableName: String? = nil, bundle: Bundle = Bundle.main, value: String = "") -> String{
+        NSLocalizedString(self, tableName: tableName, bundle: bundle, value: value, comment: comment)
+    }
+    
+    func localized(withDefaultValue value:String, tableName: String? = nil, bundle: Bundle = Bundle.main, comment:String = "") -> String {
+        NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: value, comment: comment)
+    }
 
     static var empty: Self {
         ""
