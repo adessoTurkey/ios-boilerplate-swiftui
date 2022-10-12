357627a216c0bfa361250033823c072928934be0
adesso-said <said.alir@adesso.com.tr>
2022-10-12 13:26:18 +0300

    syntax correction

--- boilerplate-ios-swiftui/Utility/ViewModifiers/OnFirstAppearModifier.swift
+++ boilerplate-ios-swiftui/Utility/ViewModifiers/OnFirstAppearModifier.swift
@@ -9,23 +9,24 @@
 import SwiftUI
 
 struct OnFirstAppearModifier: ViewModifier {
-    let perform:() -> Void
+
+    let perform : () -> Void
     @State private var firstTime: Bool = true
 
     func body(content: Content) -> some View {
-        content
-            .onAppear{
-                if firstTime{
-                    firstTime = false
-                    self.perform()
-                }
+        content.onAppear {
+            if firstTime {
+                firstTime = false
+                self.perform()
             }
+        }
     }
 }
 
 
 extension View {
-    func onFirstAppear( perform: @escaping () -> Void ) -> some View {
+
+    func onFirstAppear(perform: @escaping () -> Void) -> some View {
         return self.modifier(OnFirstAppearModifier(perform: perform))
     }
 }
