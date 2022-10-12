3aa86ebd1b8cb4a699d756e478ff08186c0ae4ab
adesso-said <said.alir@adesso.com.tr>
2022-10-12 09:57:58 +0300

    On First Appear View Modifier is added

---
+++ boilerplate-ios-swiftui/Utility/ViewModifiers/OnFirstAppearModifier.swift
@@ -0,0 +1,31 @@
+//
+//  OnFirstAppearModifier.swift
+//  boilerplate_ios_swiftui
+//
+//  Created by Said on 10.10.2022.
+//  Copyright © 2022 Adesso Turkey. All rights reserved.
+//
+
+import SwiftUI
+
+struct OnFirstAppearModifier: ViewModifier {
+    let perform:() -> Void
+    @State private var firstTime: Bool = true
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear{
+                if firstTime{
+                    firstTime = false
+                    self.perform()
+                }
+            }
+    }
+}
+
+
+extension View {
+    func onFirstAppear( perform: @escaping () -> Void ) -> some View {
+        return self.modifier(OnFirstAppearModifier(perform: perform))
+    }
+}
