12bf6025a21120ea1b1996a64d608e759beffe86
Fatih Ozturk <mfatihozturk00@gmail.com>
2022-10-19 13:16:03 +0300

    [BSU-0017] - Minimum iOS Target to iOS 15.0
    * Bumped minimum iOS Target to iOS 15.0

--- boilerplate-ios-swiftui/Utility/Extensions/StringExtensions.swift
+++ boilerplate-ios-swiftui/Utility/Extensions/StringExtensions.swift
@@ -10,14 +10,14 @@ import UIKit
 extension String {
 
     var localized: String {
-        NSLocalizedString(self, comment: "") //swiftlint:disable:this nslocalizedstring_key
+        NSLocalizedString(self, comment: "") // swiftlint:disable:this nslocalizedstring_key
     }
-    
-    func localized(withComment comment:String, tableName: String? = nil, bundle: Bundle = Bundle.main, value: String = "") -> String{
+
+    func localized(withComment comment: String, tableName: String? = nil, bundle: Bundle = Bundle.main, value: String = "") -> String {
         NSLocalizedString(self, tableName: tableName, bundle: bundle, value: value, comment: comment)
     }
-    
-    func localized(withDefaultValue value:String, tableName: String? = nil, bundle: Bundle = Bundle.main, comment:String = "") -> String {
+
+    func localized(withDefaultValue value: String, tableName: String? = nil, bundle: Bundle = Bundle.main, comment: String = "") -> String {
         NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: value, comment: comment)
     }
 
