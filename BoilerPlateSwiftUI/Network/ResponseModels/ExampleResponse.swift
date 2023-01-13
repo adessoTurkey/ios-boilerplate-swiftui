9425ec9d5145bfe772513c03cb2e9e8505194c2a
fatih.saglam@adesso.com.tr <Fatih Saglam>
2023-01-13 12:40:56 +0300

    Add 'ExampleResponse' inside 'ResponseModels' group

---
+++ BoilerPlateSwiftUI/Network/ResponseModels/ExampleResponse.swift
@@ -0,0 +1,13 @@
+//
+//  ExampleResponse.swift
+//  BoilerPlateSwiftUI
+//
+//  Created by Saglam, Fatih on 13.01.2023.
+//  Copyright © 2023 Adesso Turkey. All rights reserved.
+//
+
+import Foundation
+
+class ExampleResponse: Decodable {
+    var value: String?
+}
