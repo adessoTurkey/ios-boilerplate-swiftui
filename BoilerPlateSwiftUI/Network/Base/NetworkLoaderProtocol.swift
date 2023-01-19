2134fa88c9d1ae203becff35be88859c24d38955
fatih.saglam@adesso.com.tr <Fatih Saglam>
2023-01-19 17:18:46 +0300

    Extract URLRequest creation responsibility to a helper method 'prepareURLRequest'

--- BoilerPlateSwiftUI/Network/Base/NetworkLoaderProtocol.swift
+++ BoilerPlateSwiftUI/Network/Base/NetworkLoaderProtocol.swift
@@ -17,22 +17,29 @@ extension URLSession: URLSessionProtocol { }
 protocol NetworkLoaderProtocol {
     var session: URLSessionProtocol { get set }
     var decoder: JSONDecoder { get set }
-    
+
     func request<T: Decodable>(with requestObject: RequestObject, responseModel: T.Type) async throws -> T
 }
 
 extension NetworkLoaderProtocol {
     func request<T: Decodable>(with requestObject: RequestObject, responseModel: T.Type) async throws -> T {
-        guard let url = URL(string: requestObject.url) else { throw AdessoError.customError(1, "Bad url") }
+        let (data, response) = try await session.data(for: prepareURLRequest(with: requestObject), delegate: nil)
+        let successCodeRange = 200...299
+        guard let statusCode = (response as? HTTPURLResponse)?.statusCode,
+              successCodeRange.contains(statusCode) else { throw AdessoError.httpError(status: .badRequest) }
+        let decodedData = try decoder.decode(responseModel, from: data)
+        return decodedData
+    }
+
+    private func prepareURLRequest(with requestObject: RequestObject) throws -> URLRequest {
+        guard let url = URL(string: requestObject.url) else {
+            throw AdessoError.customError(1, "Bad url")
+        }
         var request = URLRequest(url: url)
         request.httpMethod = requestObject.method.rawValue
         request.allHTTPHeaderFields = requestObject.headers
         request.httpBody = requestObject.body
-        let (data, response) = try await session.data(for: request, delegate: nil)
-        let successCodeRange = 200...299
-        guard let statusCode = (response as? HTTPURLResponse)?.statusCode,
-              successCodeRange.contains(statusCode) else { throw AdessoError.badResponse }
-        let decodedData = try decoder.decode(responseModel, from: data)
-        return decodedData
+
+        return request
     }
 }
